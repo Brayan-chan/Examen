f10daee647bce82f87af90c32b4c82cb3b759980
Brayan-chan <al071392@uacam.mx>
2024-06-05 21:50:23 -0600

    change the styles and implement logic

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    //Seleccionamos los id de los elementos
-    const rojo = document.getElementById('rojo')
+document.addEventListener('DOMContentLoaded', function () {
+    const modelo = document.getElementById('modelo');
+    const inputColores = document.querySelectorAll('input[name="color"]');
+    const inputsAccesorios = document.querySelectorAll('input[name="accesorio"]');
 
-    const azul = document.getElementById('azul')
+    const modeloSeleccionado = document.getElementById('modelo-seleccionado');
+    const colorSeleccionado = document.getElementById('color-seleccionado');
+    const accesoriosSeleccionados = document.getElementById('accesorios-seleccionados');
 
-    const info = document.querySelector('.info')
+    modelo.addEventListener('change', function () {
+        modeloSeleccionado.textContent = modelo.value;
+    });
 
-    const coche = document.getElementById('coche')
+    inputColores.forEach(input => {
+        input.addEventListener('change', function () {
+            colorSeleccionado.textContent = input.value;
+            colorSeleccionado.className = 'seleccion ' + input.value.toLowerCase();
+        });
+    });
 
-    azul.addEventListener('click', function(evento) {
-        evento.preventDefault()
-        info.innerHTML =
-        '<p>El color de tu coche es <span class="azul">azul</span></p>'
-        coche.innerHTML =
-        '🚙'
-    })
-
-    rojo.addEventListener('')
-})
+    inputsAccesorios.forEach(input => {
+        input.addEventListener('change', function () {
+           //accesoriosSeleccionados
+        });
+    });
+});
